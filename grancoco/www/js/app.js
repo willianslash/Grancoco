1a2f76061bdbc3222bf370020c93de3bbbf19c15
Ecletica Informatica <ecleticainformatica@MacBook-Pro-de-Ecletica-1056.local>
2016-10-07 18:33:35 -0300

    Alterado a cor de fundo do app

--- grancoco/www/js/app.js
+++ grancoco/www/js/app.js
@@ -25,7 +25,10 @@ angular.module('Grancoco', ['ionic', 'Grancoco.controllers', 'Grancoco.services'
 
 
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+
+  $ionicConfigProvider.tabs.position('bottom');
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
